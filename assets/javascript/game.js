ba7ff6d70ab327084f74a40cb5576859fca74be8
Terrance Bryant <tj904615@gmail.com>
2019-08-17 22:23:51 -0500

    Creating variables using Const

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+const randomResult;
+const losses;
+const wins;
+
+
+
+//Every Crystal needs to have a random number between 1-12
+//Everytime we win or lose a random number should be generated
+//Score should increase when Crystals are clicked until the final score is equal
+//If final score isn't equal, start over
+
