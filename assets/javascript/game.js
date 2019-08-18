2c5398a64c423229fd04c671d96e51b54fc669ee
Terrance Bryant <tj904615@gmail.com>
2019-08-17 23:51:13 -0500

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,98 @@
-const randomResult;
-const losses;
-const wins;
+$(document).ready(function(){
+   
+
+    // document.getElementById("randomNumGenerator").innerHTML = Math.floor(Math.random() * 120) +19;
+    var winText = document.getElementById("win-count");
+    var lossText = document.getElementById("loss-count");
+    // var targetNumber = document.getElementById("randomNumGenerator");
+    var tarNum = Math.floor(Math.random() * 120) +19;
+    var userGuess = document.getElementById("totalScore");
+    document.getElementById("randomNumGenerator").innerHTML = tarNum;
 
 
+    $("randomNumGenerator").text(tarNum);
 
-//Every Crystal needs to have a random number between 1-12
-//Everytime we win or lose a random number should be generated
-//Score should increase when Crystals are clicked until the final score is equal
-//If final score isn't equal, start over
+function generateRandomNumber(){
+    return Math.floor(Math.random() * 12) +1; 
+    }
+var totalCount = 0;
+var button1RandomNumber = generateRandomNumber();
+var button2RandomNumber = generateRandomNumber();
+var button3RandomNumber = generateRandomNumber();
+var button4RandomNumber = generateRandomNumber();
 
+$("#button-1").on("click", function(){
+    totalCount += button1RandomNumber
+    // document.getElementById("totalScore").innerHTML = totalCount;
+    totalDisplay();
+    checkIfGameOver();
+    
+});
+
+$("#button-2").on("click", function(){
+    totalCount += button2RandomNumber
+    // document.getElementById("totalScore").innerHTML = totalCount;
+    totalDisplay();
+    checkIfGameOver();
+    
+});
+
+$("#button-3").on("click", function(){
+    totalCount += button3RandomNumber
+    // document.getElementById("totalScore").innerHTML = totalCount;
+    totalDisplay();
+    checkIfGameOver();
+    
+});
+
+$("#button-4").on("click", function(){
+    totalCount += button4RandomNumber
+    // document.getElementById("totalScore").innerHTML = totalCount;
+    totalDisplay();
+    checkIfGameOver();
+   
+});
+console.log(totalCount);
+console.log(tarNum);
+
+function totalDisplay(){
+    document.getElementById("totalScore").innerHTML = totalCount;
+
+}
+
+
+function gameReset(){
+    tarNum = Math.floor(Math.random() * 120) + 19;
+    document.getElementById("randomNumGenerator").innerHTML = tarNum;
+    totalCount=0;
+    totalDisplay();
+    button1RandomNumber = generateRandomNumber();
+    button2RandomNumber = generateRandomNumber();
+    button3RandomNumber = generateRandomNumber();
+    button4RandomNumber = generateRandomNumber();
+    
+}
+
+
+function checkIfGameOver(){
+if (totalCount === tarNum) {
+    winText.textContent++;
+    console.log(totalCount);
+    alert("You Win!")
+    gameReset();
+   
+
+}
+
+else if (totalCount > tarNum) {
+    lossText.textContent++;
+    console.log(totalCount);
+    console.log(tarNum);
+    alert("You lose!, Try again!")
+    gameReset();
+        
+}
+
+
+}
+});
